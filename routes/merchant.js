0d996a658ba2cddad645d1f42657659dc64a06c2
Jermaine Watkins <jermainewatkins@Jermaines-MacBook-Pro.local>
2017-10-14 18:58:41 -0500

    wip

--- routes/merchant.js
+++ routes/merchant.js
@@ -2,14 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    console.log("Request handler random was called.");
+    var merchantId = getMerchantIdFromLinks(links);
+    var fraudCount = merchantId === null ? 0 : getFraudCountFromMerchantId(merchantId) ;
     res.writeHead(200, {"Content-Type": "application/json"});
-    var otherArray = randomIntFromInterval(0,100);
     var json = JSON.stringify({
-        fraud_count: otherArray
+        fraud_count: fraudCount
     });
     res.end(json);
 });
+
+function getMerchantIdFromLinks(links){
+    //  api call to see if link return merchant
+    MerchantId = 123123123;
+    return MerchantId
+}
+
+function getFraudCountFromMerchantId(MerchantId){
+
+    var count = randomIntFromInterval(0,100);
+    //api with the merchant id return alert count
+    //count the events
+
+    return count;
+}
 
 function randomIntFromInterval(min,max)
 {
