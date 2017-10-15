bde9e17cd06c54a463baaec69e2b30a2b26f74ad
teij04 <teij04@users.noreply.github.com>
2017-10-15 12:33:46 -0400

    changes color on click

--- Extension/test.js
+++ Extension/test.js
@@ -4,14 +4,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         function updateIcon(){
 
             chrome.tabs.getSelected(null, function(tab) {
-                let numberReports = getFraudCount(tab.url).then(function(val) {
-                    console.log(val);
-                }).catch(function(err) {
-                    console.log(err);
-                });
-
-                alert(numberReports);
-
+				var currentURL = tab.url;
+				var numberReports = 22; 
                 if (numberReports >= 60){
                     chrome.browserAction.setIcon({path:"icon1.png"});
                 }else if (numberReports >= 30){
@@ -23,59 +17,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
             });
         }
 
-        function getFraudCount(url){
-
-            var details = {
-                domainName: tab.url
-            };
-
-            var formBody = [];
-            for (var property in details) {
-                var encodedKey = encodeURIComponent(property);
-                var encodedValue = encodeURIComponent(details[property]);
-                formBody.push(encodedKey + "=" + encodedValue);
-            }
-            formBody = formBody.join("&");
-
-            var count = fetch('http://localhost:3000/merchant', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: formBody
-            });
-
-
-            var url = process.env.WHOIS_API_ENDPOINT;
-            var username = process.env.WHOIS_USER;
-            var password = process.env.WHOIS_PASS;
-
-            var parameters = {
-                domainName: domain,
-                username: username,
-                password: password,
-                outputFormat: 'json'
-            };
-
-            url = url + querystring.stringify(parameters);
-            return new Promise(function(resolve, reject){
-                request( url , function (error, response, body) {
-                    // in addition to parsing the value, deal with possible errors
-                    if (error) return reject(error);
-                    try {
-                        // JSON.parse() can throw an exception if not valid JSON
-                        resolve(JSON.parse(body).WhoisRecord.registrant.organization);
-                    } catch(e) {
-                        reject(e);
-                    }
-                });
-            });
-        }
-
-        //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
         chrome.tabs.executeScript({
-            code:  updateIcon()
-        })
-    }
-);
+            code: updateIcon()
+                });
+        
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "https://www.weaklinks.co/merchant", true);
+		xhr.setRequestHeader('DomainName', 'currentURL');
+		xhr.onload = function () {
+			// do something to response
+			console.log(this.responseText);
+			};
+		xhr.send();
+
+		console.log(xhr.status);
+		console.log(xhr.statusText);
+		
+});
