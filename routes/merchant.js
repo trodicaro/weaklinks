dd83aa78917eb0c443820e5ea2fbabee91137558
Jermaine Watkins <jermainewatkins@Jermaines-MacBook-Pro.local>
2017-10-14 23:56:20 -0500

    Revert "WIP"
    This reverts commit ec83f44a2c14b95c3c86c65a51ab5577a24870d8.

--- routes/merchant.js
+++ routes/merchant.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    links = ['helllo'];
     var merchantId = getMerchantIdFromLinks(links);
     var fraudCount = merchantId === null ? 0 : getFraudCountFromMerchantId(merchantId) ;
     res.writeHead(200, {"Content-Type": "application/json"});
@@ -19,20 +18,6 @@ function getMerchantIdFromLinks(links){
 }
 
 function getFraudCountFromMerchantId(MerchantId){
-
-    var ClientOAuth2 = require('client-oauth2')
-
-    var Auth = new ClientOAuth2({
-        clientId: 'l7xx27729b45a68c4a849160b2cbb9cf2a35',
-        clientSecret: '582dcdd36d324e0b91e61f9b15013db7',
-        accessTokenUri: 'https://apis.discover.com/auth/oauth/v2/token',
-        scopes: ['FRAUDALERTS']
-    });
-
-    Auth.credentials.getToken()
-        .then(function (user) {
-            console.log(user.data.access_token) //=> { accessToken: '...', tokenType: 'bearer', ... }
-        })
 
     var count = randomIntFromInterval(0,100);
     //api with the merchant id return alert count
